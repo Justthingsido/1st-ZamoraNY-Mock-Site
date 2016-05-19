baf24de229f09429e022ede98271cba669d94078
Guillermo <guillermo@justthingsido.com>
2016-05-19 19:48:59 -0400

    scrolling

--- assets/_js/smoothScrolling.js
+++ assets/_js/smoothScrolling.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
 			$('nav.site-nav a, .landing-nav a, .mobile-nav a').each(function () {
 				$(this).removeClass('current');
-			})
+			});
 			$(this).addClass('current');
 
 			var target = this.hash;
@@ -51,7 +51,9 @@ $(document).ready(function () {
 			if(delay) return;
 
 			delay = true;
-			setTimeout(function(){delay = false},200)
+			setTimeout(function(){
+				delay = false;
+			},200);
 
 			var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
 
@@ -76,36 +78,36 @@ $(document).ready(function () {
 
 
 
-	// ------------------------------------
-	// mosue-scroll
-
-	(function() {
-		var delay = false;
-
-		$(document).on('mousewheel DOMMouseScroll', function(event) {
-			event.preventDefault();
-			if(delay) return;
-
-			delay = true;
-			setTimeout(function(){delay = false},200)
-
-			var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-
-			var a= document.getElementsByClassName('inner');
-			if(wd < 0) {
-				for(var i = 0 ; i < a.length ; i++) {
-					var t = a[i].getClientRects()[0].top;
-					if(t >= 40) break;
-				}
-			}
-			else {
-				for(var i = a.length-1 ; i >= 0 ; i--) {
-					var t = a[i].getClientRects()[0].top;
-					if(t < -20) break;
-				}
-			}
-			$('html,body').animate({
-				scrollTop: a[i].offsetTop
-			});
-		});
-	})();
+	// // ------------------------------------
+	// // mosue-scroll
+	//
+	// (function() {
+	// 	var delay = false;
+	//
+	// 	$(document).on('mousewheel DOMMouseScroll', function(event) {
+	// 		event.preventDefault();
+	// 		if(delay) return;
+	//
+	// 		delay = true;
+	// 		setTimeout(function(){delay = false;},200);
+	//
+	// 		var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
+	//
+	// 		var a= document.getElementsByClassName('inner');
+	// 		if(wd < 0) {
+	// 			for(var i = 0 ; i < a.length ; i++) {
+	// 				var t = a[i].getClientRects()[0].top;
+	// 				if(t >= 40) break;
+	// 			}
+	// 		}
+	// 		else {
+	// 			for(var i = a.length-1 ; i >= 0 ; i--) {
+	// 				var t = a[i].getClientRects()[0].top;
+	// 				if(t < -20) break;
+	// 			}
+	// 		}
+	// 		$('html,body').animate({
+	// 			scrollTop: a[i].offsetTop
+	// 		});
+	// 	});
+	// })();
